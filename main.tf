2c4514b24d3aa130ed7dc3b08a1d9ce4791cc7d7
Breee <loeffler.uni@gmail.com>
2020-10-13 21:13:38 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "terraform-providers/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_container" "nginx" {
+  image = "nginx:1.18.0"
+  name  = "tutorial"
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+  labels {
+    label = "traefik.http.routers.tutorial-nginx.rule"
+    value = "Host(`tutorial-nginx.example.com`)"
+  }
+  labels {
+    label = "traefik.http.services.tutorial-nginx.loadbalancer.server.port"
+    value = "80"
+  }
+  networks_advanced {
+    name = "proxy"
+  }
+}
